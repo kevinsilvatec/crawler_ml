a01d1d61da5efa0750b75ea2d2f85c8fe0a48b8a
Kevin Silva <kevin.silva@grupolpj.com>
2020-05-02 04:32:40 -0300

    Finalização do retorno

--- src/Controller/CrawlerController.js
+++ src/Controller/CrawlerController.js
@@ -1,160 +1,97 @@
 const request = require('request');
-const cheerio = require('cheerio');
+const cheerioHelper = require('../Helpers/cheerio-helper');
+const fileHelper = require('../Helpers/file-helper');
 
 module.exports = {
     search(req, res){
 
         let {search, limit} = req.body;
 
+        if(!Number.isInteger(limit)){
+            res.json({
+                code: "500",
+                message: "O campo limit deve ser um número!"
+            });
+        }
+
+        if(limit > 50){
+            res.json({
+                code: "500",
+                message: "O campo tem tamanho máximo de 50!"
+            });
+        }
+
+
         var url = `https://lista.mercadolivre.com.br/${search}#D[A:${search}]`;
-        
-        callbackPrimaryBody = (url, body) => {
-            let adsInitialArray = createAdsInitialArray(body, limit);        
+
+        callbackPrimaryBody = (body, adId) => {
+            let arrAdsInitial = cheerioHelper.createAdsInitialArray(body, limit);
+            arrAdsInitial.forEach(e => {
+                requestBody(e.link, callbackAdsBody, e.id);
+            });    
+
+            let urlPrincipal = './json/tempPrincipal.json.txt';
+            let urlStores = './json/tempStores.json.txt';
+            let fileErrMessage = "Ocorreu um erro ao criar o arquivo temporário!!";            
             
-            adsInitialArray.forEach(e => {
-                requestBody(e.link, callbackAdsBody);
-            });
-
+            fileHelper.createFile(urlPrincipal, JSON.stringify(arrAdsInitial, null, 4), errCallback, fileErrMessage);
+            fileHelper.createFile(urlStores, '', errCallback, fileErrMessage);
         }
 
-        callbackAdsBody = (url, body) => {
-            var arrayUrlDataStore = createArrayUrlDataStore(url, body);            
-            arrayUrlDataStore.forEach(e => {
-                requestBody(e.linkStore, callbackStoresBody);
+        callbackAdsBody = (body, adId) => {
+            var arrUrlDataStore = cheerioHelper.createArrayUrlDataStore(body);   
+            
+            arrUrlDataStore.forEach(e => {
+                requestBody(e.linkStore, callbackStoresBody, adId, limit);                
             });
         }
 
-        callbackStoresBody = (url, body) => {
-            var arrayLocationStore = createArrayLocationStore(url, body);
-            console.log(arrayLocationStore);
+        callbackStoresBody = (body, adId) => {
+            var arrLocationStore = cheerioHelper.createArrayLocationStore(body, adId);   
+            let url = './json/tempStores.json.txt';
+            let fileErrMessage = "Ocorreu um erro ao adicionar dados ao arquivo temporário!";
+            fileHelper.appendFile(url, JSON.stringify(arrLocationStore, null, 4), errCallback, fileErrMessage);
+            
         }       
         
-        requestBody(url, callbackPrimaryBody);
-        
-    }
-}
-
-function createArrayLocationStore(url, body){
-    let $ = cheerio.load(body);
-    arrLocationStore = [];
-    let nameStore = $('#store-info__name').text();  
-    let locationStore =   $('.location-subtitle').text();
-    let stateStore;
-    
-    if(!locationStore){
-        stateStore = null;
-    }else{
-        stateStore = unlinkState(locationStore);
-    }
-
-    if(!nameStore){
-        nameStore = $(".breadcrumb__title").text();
-    }
-
-    const obj = { 
-        nameStore : nameStore,
-        stateStore: stateStore        
-    }; 
-
-    arrLocationStore.push(obj);
-    return arrLocationStore;
-
-}
-
-function createArrayUrlDataStore(url, body){
-    let $ = cheerio.load(body);
-    let arrStore = [];
-
-    //Caso vendedor
-    let urlDataStore = $('#seller-view-more-link').attr("href");
-    let amountElements = $('.ui-pdp-seller__header__subtitle').length;
-    
-    //caso loja
-    if(!urlDataStore){
-        $('.ui-pdp-seller__header__subtitle').each(function(){
-            
-            if(amountElements > 1){
-                //caso loja oficial
-                let store = $(this).parent().find('.ui-pdp-action-modal__link').text().toLowerCase();
-                store = removeSpaces(store);
-                urlDataStore = `https://loja.mercadolivre.com.br/${store}`;
-            }else{
-                //caso loja apenas
-                let store = $(this).parent().find('.ui-pdp-seller__link-trigger').text().toUpperCase();
-                urlDataStore = `https://perfil.mercadolivre.com.br/${store}`;
-            }
-            
-        });
-    }
-
-    const obj = { 
-        linkStore : urlDataStore        
-    }; 
-    
-    arrStore.push(obj);
-    return arrStore;
-}
-
-function unlinkState(location){
-    location = location.split(',');
-    
-    location = location[1].split('.');
-
-    return location[0];
-}
-
-function removeSpaces(string){
-    string = string.replace(" ", "-");
-    return string;
-}
-
-function createAdsInitialArray(body, limit){
-    const arrAds = []; 
-    
-    let $ = cheerio.load(body); 
-    //VERIFICAÇÃO DE LIMIT DEVERÁ SER FEITA AQUI PARA INTEGRAR A PAGINAÇÃO
-    //USAR O THIS.LENGTH
-
-    $('#searchResults li.article').each(function(index){
-        let nameAd = $(this).find('.list-view-item-title').text();
-        let linkAd =  $(this).find('.item__info-link').attr('href');
-        if(!linkAd){
-            linkAd = $(this).find('.item__info-title').attr('href');
-        }
-        let decimal = $(this).find('div.item__price > span.price__decimals').text();
-        let priceAd = $(this).find('div.item__price > span.price__fraction').text();
-        
-        
-        priceAd = priceAd.replace('.', '') + (decimal != '' ? '.' + decimal : '.00');
-        
-        const obj = { 
-            name : nameAd, 
-            link : linkAd,
-            price: priceAd
-        }; 
-
-        arrAds.push(obj);
-        
-        if(index == limit -1){
-            return false;
+        errCallback = (message, err) => {
+            res.json({
+                message: message,
+                erro: err
+            });
         }
 
-    });
+        finalCallback = () => {
+            let firstFileUrl = './json/tempPrincipal.json.txt';
+            let secondFileUrl = './json/tempStores.json.txt';
+            let finalObject = fileHelper.mergeFiles(firstFileUrl, secondFileUrl);
+            // console.log(finalObject);
+            res.json(finalObject);
+        }
 
-    return arrAds;
-
+        requestBody(url, callbackPrimaryBody, null);
+    }
 }
 
-function requestBody(url, callback){
+function requestBody(url = null, _callback, adId = null, limit = null){
     request(url, function (err, res, body) {
         
         if(err){
-            console.log(err, "error occured while hitting URL");
+            let errMessage = 'Ocorreu um erro ao tentar capturar a página';
+            errCallback(errMessage, err);
         }
-        else{
-            if(callback){
-                callback(url, body);
-            }
+        else{            
+            _callback(body, adId);
+
         }
+
+        let url = './json/tempStores.json.txt';
+        if(limit){
+            let amountItems = fileHelper.countItemsFile(url, limit);
+            if(amountItems == limit){
+                finalCallback();
+            }        
+        }
+        
     });
 }
